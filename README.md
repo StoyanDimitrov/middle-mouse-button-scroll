c3f8ec7952036e4cada08592ee32b9fb782a005e
Стоян Димитров <stoyan@easyads.bg>
2019-05-14 14:03:41 +0300

    High precision mode holding down the  key while scrolling

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Scroll fast through long documents with middle-click scroll.
 
-Pressing `Alt`, `Ctrl` or `Shift` key when using the mouse button enables you to use the browser's implementation of middle-click scroll.
+Holding down `Alt` or `Ctrl` key with pressed middle mouse button enables you to use the browser's implementation of middle-click scroll.
+Holding down `Shift` key with pressed middle mouse button enables you to scroll with high precision.
 
-**Note**: For now the extension scrolls only the whole page and not different parts of it. If you are unable to scroll given page that's the reason why. That feature is planned.
+**Note**: For the time being the extension scrolls only the whole page and not different parts of it. If you are unable to scroll given page that's the reason why. That feature is planned.
+Also, for the time being the keyboard keys are non configurable. The current implementation of Firefox for configurable hotkeys can not use only modifier key (`Ctrl`, `Alt`, `Shift`, `Meta` etc.) for shorckuts.
--- content_scripts/scroll.js
+++ content_scripts/scroll.js
@@ -1,58 +1,126 @@
-var ratio = {}
+(function() {
+  function scrollHandler() {
 
+    function scrollRatio(event) {
+      let ratio = .8
 
-function setRatio()
-{
-  ratio = {
-    x: document.body.scrollWidth / (window.innerWidth * .8),
-    y: document.body.scrollHeight / (window.innerHeight * .8)
+      if (event.shiftKey) {
+        ratio = 12
+      }
+
+      return ratio
+    }
+
+    function scrollAmount(event) {
+      const amount = {
+        x: 0,
+        y: 0,
+      }
+      const ratio = scrollRatio(event)
+
+      if (window.scrollMaxX !== 0) {
+        let reachedLeft = false
+          , reachedRight = false
+        const scrollingLeft = event.movementX < 0
+        const scrollingRight = event.movementX > 0
+
+        if (window.scrollX === 0) {
+          reachedLeft = true
+        }
+        if (window.scrollX === window.scrollMaxX) {
+          reachedRight = true
+        }
+
+        if ((! reachedLeft && scrollingLeft)
+          || (! reachedRight && scrollingRight)
+        ) {
+          const documentWidth = Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth)
+
+          amount.x = Math.floor(event.movementX * (documentWidth / (window.innerWidth * ratio)))
+        }
+      }
+
+      if (window.scrollMaxY !== 0) {
+        let reachedTop = false
+          , reachedBottom = false
+        const scrollingUp = event.movementY < 0
+        const scrollingDown = event.movementY > 0
+
+        if (window.scrollY === 0) {
+          reachedTop = true
+        }
+        if (window.scrollY === window.scrollMaxY) {
+          reachedBottom = true
+        }
+
+        if ((! reachedTop && scrollingUp)
+          || (! reachedBottom && scrollingDown)
+        ) {
+          const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight)
+
+          amount.y = Math.floor(event.movementY * (documentHeight / (window.innerHeight * ratio)))
+        }
+      }
+
+      return amount
+    }
+
+    return function(event) {
+      const scrollBy = scrollAmount(event)
+
+      if (scrollBy.x === 0
+        && scrollBy.y === 0
+      ) {
+        return
+      }
+
+      window.scrollBy(scrollBy.x, scrollBy.y)
+    }
   }
-}
 
-function scroll(event)
-{
-  setCursor('move')
-  window.scrollBy(Math.ceil(event.movementX * ratio.x), Math.ceil(event.movementY * ratio.y))
-}
+  const handleScroll = scrollHandler()
 
-function setCursor(cursor)
-{
-  if (cursor) {
-    return document.body.style.cursor = cursor
+  function setCursor(cursor) {
+    if (cursor) {
+      return document.body.style.cursor = cursor
+    }
+
+    document.body.style.removeProperty('cursor')
+
+    if (document.body.style.length === 0) {
+      document.body.removeAttribute('style')
+    }
   }
 
-  document.body.style.removeProperty('cursor')
+  window.addEventListener('mousedown', (event) => {
+    if (event.altKey
+      || event.ctrlKey
+    ) {
+      return
+    }
 
-  if (document.body.style.length === 0) {
-    document.body.removeAttribute('style')
-  }
-}
+    // middle button click
+    if (event.button !== 1
+      && event.buttons !== 4
+    ) {
+      return
+    }
 
-window.addEventListener('resize', setRatio)
+    // no scroll
+    if (window.scrollMaxX === 0
+      && window.scrollMaxY === 0
+    ) {
+      return
+    }
 
-window.addEventListener('mousedown', (event) => {
+    event.preventDefault()
 
-  if (event.altKey || event.ctrlKey || event.shiftKey) {
-    return
-  }
+    setCursor('move')
+    window.addEventListener('mousemove', handleScroll, true)
+  })
 
-  if (event.button !== 1 && event.buttons !== 4) {
-    return
-  }
-
-  if (window.scrollMaxY === 0 && window.scrollMaxX === 0) {
-    return
-  }
-
-  event.preventDefault()
-
-  setRatio()
-
-  window.addEventListener('mousemove', scroll, true)
-})
-
-window.addEventListener('mouseup', () => {
-
-  setCursor()
-  window.removeEventListener('mousemove', scroll, true)
-})
+  window.addEventListener('mouseup', () => {
+    setCursor()
+    window.removeEventListener('mousemove', handleScroll, true)
+  })
+}())
